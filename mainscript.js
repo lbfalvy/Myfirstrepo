b77fa01fc8d36a4d192347cf5ffaa5944c83b352
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2022-08-15 21:13:40 +0200

    Javascript refactor

--- mainscript.js
+++ mainscript.js
@@ -1,61 +1,40 @@
-document.onload = createListBtn();
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 //current date:
-const y = new Date();
-document.getElementById("year").innerHTML = y.getFullYear();
-
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const m = new Date();
-let month = months[m.getMonth()];
+const now = new Date();
+document.getElementById("year").innerHTML = now.getFullYear();
+let month = MONTHS[now.getMonth()];
 document.getElementById("month").innerHTML = month;
+document.getElementById("day").innerHTML = now.getDate();
 
-const d = new Date();
-document.getElementById("day").innerHTML = d.getDate();
+const canSitOnGround = MONTHS.filter(month => !month.includes("r"))
 
-//creating buttons on list items
-
-function createListBtn(){
-    let todoList = document.getElementsByTagName("li");
-    for (let i = 0; i < todoList.length; i++){
-        let fBtn = document.createElement("button");
-        fBtn.className = "fBtn";
-        let dBtn = document.createElement("button");
-        dBtn.className = "dBtn";
-        todoList[i].appendChild(fBtn);
-        todoList[i].appendChild(dBtn);
+/**
+ * Construct and append new todo element to list
+ * @param {string} title 
+ * @param {string} details 
+ */
+function addTodo(title, details) {
+    // Create list item
+    const li = document.createElement("li");
+    // Generate buttons
+    const fBtn = document.createElement("button");
+    fBtn.className = "fBtn";
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "dBtn";
+    deleteBtn.onclick = function () {
+        li.style.display = "none";
     }
-    //click on finish button to remove item 
-    
-    let close = document.getElementsByClassName("dBtn");
-    for (let i = 0; i < close.length; i++){
-        close[i].onclick = function() {
-            let li = this.parentElement;
-            li.style.display = "none";
-            
-        }
-    }
+    li.append(title, details, fBtn, deleteBtn);
+    todoList.append(li);
 }
 
-
-let x = "";
-let z = "";
-function addTodo(){
-    x = document.getElementById("myInput").value;
-    z = document.getElementById("details").value;
-   let newLi = document.createElement("li");
-   let newTodo = document.createTextNode(x);
-   let newDetail = document.createTextNode(z);
-   newTodo.classname = "newTodo";
-   newDetail.classname = "newDetail";
-   newLi.appendChild(newTodo);
-   newLi.appendChild(newDetail);
-   todoList.appendChild(newLi);
-   createListBtn();
-  
-   
+const TITLE_INPUT = document.getElementById("myInput");
+const DETAILS_INPUT = document.getElementById("details");
+function onAddClick() {
+    addTodo(TITLE_INPUT.value, DETAILS_INPUT.value);
 }
 
-
-
-
-
+addTodo("van még sok feladatom", "")
+addTodo("van még sok feladatom", "")
+addTodo("vajon hol nézem el a js codot?", "")
